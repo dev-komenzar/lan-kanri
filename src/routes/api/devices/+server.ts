9cb7d5c0c9ef7f6cf5a3483589c54c15f509bbfd
dev-komenzar <takuya.kometan@gmail.com>
2026-09-14 16:48:24 +0900

    Fix getMacAddresses call site

--- src/routes/api/devices/+server.ts
+++ src/routes/api/devices/+server.ts
@@ -1,11 +1,9 @@
-import { NOTION_DEVICE_DATABASE, NOTION_USER_DATABASE } from '$env/static/private';
-import { getAllowedUsersAsync, getDevices, getMacAddresses } from '../../../utils/devices';
+import { NOTION_DEVICE_DATABASE } from '$env/static/private';
+import { getMacAddresses } from '../../../utils/devices';
 import type { RequestHandler } from './$types';
 
 export const GET = (async () => {
-	const allowedUsers = await getAllowedUsersAsync(NOTION_USER_DATABASE);
-	const devices = getDevices(allowedUsers);
-	const addresses = await getMacAddresses(NOTION_DEVICE_DATABASE, devices);
+	const addresses = await getMacAddresses(NOTION_DEVICE_DATABASE);
 
 	return new Response(JSON.stringify(addresses));
 }) satisfies RequestHandler;
